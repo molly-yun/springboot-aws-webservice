5a95e6e4860ef94a3c2191bf0d7bbde5a41df863
yuninjeong <yijeong0918@gmail.com>
2022-03-26 19:44:17 +0900

    HelloResponseDto, HelloResponseDtoTest 생성

---
+++ src/main/java/com/molly/book/springboot/web/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.molly.book.springboot.web.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
