3bcca0e0036ea9fc18f0738f50facb019b6412c3
molly-yun <mollyyun91@gmail.com>
2022-04-16 20:04:26 +0900

    delete 기능 추가

--- src/main/java/com/molly/book/springboot/web/service/PostsService.java
+++ src/main/java/com/molly/book/springboot/web/service/PostsService.java
@@ -31,6 +31,13 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);  // JpaRepository의 delete 메소드 활용
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
